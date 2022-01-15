cbcfc75e07154e50e680ca902a891bd41616280d
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-15 11:07:11 +0700

    Skeleton code for datasetloader

---
+++ s2vt/dataset.py
@@ -0,0 +1,14 @@
+import os
+from torchvision.io import read_image
+from torch.utils.data import Dataset
+
+
+class MSVDDataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
