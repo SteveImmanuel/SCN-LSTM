83c8525cfc3eebb3d0e713fdfdada15c61ad53b5
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-16 13:14:31 +0700

    Change format

--- helper.py
+++ helper.py
@@ -1,8 +1,8 @@
 def print_batch_loss(loss_value: float, current_batch: int, total_batch: int):
     print(' ' * 80, end='\r')
-    print(f'Step [{current_batch}/{total_batch}] Loss: {loss_value}', end='\r')
+    print(f'Step [{current_batch}/{total_batch}] Loss: {loss_value:.5f}', end='\r')
 
 
 def print_test_overfit(epoch_loss_value: float, current_epoch: int, total_epoch: int):
     print(' ' * 80, end='\r')
-    print(f'Epoch [{current_epoch}/{total_epoch}] Loss: {epoch_loss_value}', end='\r')
+    print(f'Epoch [{current_epoch}/{total_epoch}] Loss: {epoch_loss_value:.5f}', end='\r')
