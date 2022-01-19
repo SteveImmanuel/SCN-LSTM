7f54438fd311c89b29c0801dc8bf22df6e61db50
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-19 10:56:59 +0700

    Change mean and std based on docs

--- s2vt/constant.py
+++ s2vt/constant.py
@@ -5,5 +5,7 @@ PAD_TAG = '<PAD>'
 EOS_TAG = '<EOS>'
 BOS_TAG = '<BOS>'
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-VGG_MEAN = [103.939, 116.779, 123.68]
-VGG_STD = [1, 1, 1]
+
+# https://pytorch.org/hub/pytorch_vision_vgg/
+VGG_MEAN = [0.485, 0.456, 0.406]
+VGG_STD = [0.229, 0.224, 0.225]
