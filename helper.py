eaa9ec0ae3301459f9ec888676ae68dc9bd8e45a
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-20 22:19:43 +0700

    Change wording

--- helper.py
+++ helper.py
@@ -5,12 +5,12 @@ from s2vt.constant import EOS_TAG
 
 def print_batch_loss(loss_value: float, current_batch: int, total_batch: int):
     print(' ' * 80, end='\r')
-    print(f'Step [{current_batch}/{total_batch}] Loss: {loss_value:.5f}', end='\r')
+    print(f'Step [{current_batch}/{total_batch}] Current Loss: {loss_value:.5f}', end='\r')
 
 
 def print_test_overfit(epoch_loss_value: float, current_epoch: int, total_epoch: int):
     print(' ' * 80, end='\r')
-    print(f'Epoch [{current_epoch}/{total_epoch}] Loss: {epoch_loss_value:.5f}', end='\r')
+    print(f'Epoch [{current_epoch}/{total_epoch}] Current Loss: {epoch_loss_value:.5f}', end='\r')
 
 
 def create_batch_log_file(filepath: str) -> TextIOWrapper:
