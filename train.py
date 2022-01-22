4825406bc89a9b12fefef9ba003596b5c6608022
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-22 17:08:50 +0700

    Tweak loss function, change optimizer, change validation algorithm

--- train.py
+++ train.py
@@ -3,7 +3,8 @@ import argparse
 import torch
 import time
 import traceback
-from torch.optim.lr_scheduler import StepLR
+import math
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader
 from s2vt.utils import idx_to_annotation
 from s2vt.dataset import PreprocessedMSVDDataset
@@ -22,8 +23,6 @@ parser.add_argument('--timestep', help='Total timestep', default=80, type=int)
 parser.add_argument('--batch-size', help='Batch size for training', default=8, type=int)
 parser.add_argument('--epoch', help='Total epoch', default=20, type=int)
 parser.add_argument('--learning-rate', help='Learning rate for training', default=1e-4, type=float)
-parser.add_argument('--momentum', help='Momentum for updating gradient', default=9e-1, type=float)
-parser.add_argument('--gamma', help='Gamma for learning rate scheduler', default=5e-1, type=float)
 parser.add_argument('--model-path', help='Load pretrained model')
 parser.add_argument(
     '--test-overfit',
@@ -39,8 +38,6 @@ timestep = args.timestep
 batch_size = args.batch_size
 epoch = args.epoch
 learning_rate = args.learning_rate
-momentum = args.momentum
-gamma = args.gamma
 model_path = args.model_path
 test_overfit = args.test_overfit
 ckpt_dir = args.ckpt_dir
@@ -60,8 +57,6 @@ print('Batch size:', batch_size)
 print('Timestep:', timestep)
 print('Epoch:', epoch)
 print('Learning rate:', learning_rate)
-print('Momentum:', momentum)
-print('Gamma:', gamma)
 print('Test overfit:', test_overfit)
 
 # prepare train and validation dataset
@@ -92,10 +87,9 @@ if model_path and not test_overfit:
     print(f'\nLoading pretrained model in {model_path}\n')
     model.load_state_dict(torch.load(model_path))
 
-# optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-lr_scheduler = StepLR(optimizer, step_size=10, gamma=gamma)
-loss_func = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.999))
+lr_scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, min_lr=1e-7, verbose=True)
+loss_func = torch.nn.CrossEntropyLoss(reduction='none')
 
 if test_overfit:
     print('\nTest Overfit with Small Dataset')
@@ -138,6 +132,7 @@ else:
         epoch_loss_log_path = os.path.join(log_dir, f'{uid}_epoch_loss.csv')
         batch_loss_log = create_batch_log_file(batch_loss_log_path)
         epoch_loss_log = create_epoch_log_file(epoch_loss_log_path)
+        best_val_loss = math.inf
 
         train_dataloader_len = len(train_dataloader)
         val_dataloader_len = len(val_dataloader)
@@ -173,15 +168,14 @@ else:
                 batch_loss_log.flush()
 
             print('\n###Validation Phase###')
-            model.eval()
             for batch_idx, (X, (y, y_mask)) in enumerate(val_dataloader):
                 X = X.to(DEVICE)
                 y = y.to(DEVICE)
                 y_mask = y_mask.to(DEVICE)
-                out = model(X)
+                out = model(X, y)
 
+                temp_X = X[0:1]
                 temp_y = y[0]
-                temp_out = out[0]
 
                 out = out.view(-1, train_dataset.vocab_size)
                 y = y[:, 1:].contiguous().view(-1)
@@ -200,21 +194,31 @@ else:
             epoch_loss_log.write(f'{epoch_idx},{avg_train_loss},{avg_val_loss}\n')
             epoch_loss_log.flush()
 
-            print('###Current Result###')
+            model.eval()
+            temp_out = model(temp_X)[0]
             temp_out = torch.argmax(temp_out, dim=1).to(DEVICE).long()
             temp_out = idx_to_annotation(temp_out.tolist(), val_dataset.idx_to_word)
             temp_y = idx_to_annotation(temp_y.tolist(), val_dataset.idx_to_word)
+            print('###Current Result###')
             print('Prediction:', format_result(temp_out))
             print('Ground Truth:', format_result(temp_y[1:]))
 
             # save model checkpoint
-            if ckpt_dir and (epoch_idx % ckpt_interval == 0 or epoch_idx == epoch - 1):
-                filename = f'{uid}_epoch{epoch_idx:03}_{avg_train_loss:.3f}_{avg_val_loss:.3f}.pth'
-                filepath = os.path.join(ckpt_dir, filename)
-                torch.save(model.state_dict(), os.path.join(ckpt_dir, filename))
-                print(f'Model saved to {filepath}')
+            if ckpt_dir:
+                if (epoch_idx % ckpt_interval == 0 or epoch_idx == epoch - 1):
+                    filename = f'{uid}_epoch{epoch_idx:03}_{avg_train_loss:.3f}_{avg_val_loss:.3f}.pth'
+                    filepath = os.path.join(ckpt_dir, filename)
+                    torch.save(model.state_dict(), os.path.join(ckpt_dir, filename))
+                    print(f'Model saved to {filepath}')
 
-            # lr_scheduler.step()
+                if avg_val_loss < best_val_loss:
+                    best_val_loss = avg_val_loss
+                    filename = f'{uid}_best_weights.pth'
+                    filepath = os.path.join(ckpt_dir, filename)
+                    torch.save(model.state_dict(), os.path.join(ckpt_dir, filename))
+                    print(f'Model saved to {filepath}')
+
+            lr_scheduler.step(avg_val_loss)
 
     except Exception:
         traceback.print_exc()
@@ -229,6 +233,4 @@ else:
         batch_loss_log.close()
         epoch_loss_log.close()
 
-# python train.py --annotation-path "D:/ML Dataset/MSVD/annotations.txt" --train-data-dir "C:/MSVD_extracted/train" --val-data-dir "C:/MSVD_extracted/validation" --batch-size 8 --epoch 5 --learning-rate 1e-3
-
-# python train.py --annotation-path "D:/ML Dataset/MSVD/annotations.txt" --train-data-dir "C:/MSVD_extracted/train" --val-data-dir "C:/MSVD_extracted/validation" --batch-size 8 --epoch 10 --learning-rate 1e-3 --momentum 0.9 --gamma 0.5
+# python train.py --annotation-path "D:/ML Dataset/MSVD/annotations.txt" --train-data-dir "C:/MSVD_extracted/out/train" --val-data-dir "C:/MSVD_extracted/out/validation" --batch-size 10 --epoch 100 --learning-rate 1e-4 --ckpt-interval 10
