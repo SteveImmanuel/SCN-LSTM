0d3f443865d8f51496defaa7266523af8584ba1e
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-30 00:23:32 +0700

    Add dataset for SDN

--- savc/dataset.py
+++ savc/dataset.py
@@ -1,11 +1,8 @@
 import os
-import random
-from matplotlib import transforms
-import numpy as np
 import math
 import torch
 import cv2
-from torchvision.transforms import Compose
+import numpy as np
 from torchvision.io import read_image
 from torch.utils.data import Dataset
 from typing import List
@@ -70,3 +67,46 @@ class SequenceImageMSVDDataset(Dataset):
         tensor_frames = torch.cat(frames, dim=1)
 
         return (tensor_frames, torch.empty(0))  # 16, C, H, W
+
+
+class CNNExtractedMSVD(Dataset):
+    def __init__(
+        self,
+        annotation_file: str,
+        root_path: str,
+        cnn_2d_model: str = 'regnetx32',
+        cnn_3d_model: str = 'shufflenetv2',
+    ) -> None:
+        super().__init__()
+        self.cnn_2d_model = cnn_2d_model
+        self.cnn_3d_model = cnn_3d_model
+        self.root_path = root_path
+        self.video_dict = build_video_dict(annotation_file, reverse_key=True)
+        self.word_to_idx, self.idx_to_word, self.video_caption_mapping = build_vocab(annotation_file)
+        self.tag_dict = build_tags(annotation_file, reverse_key=True)
+        self.videos = list(set(map(lambda x: x[5:9], os.listdir(self.root_path))))
+
+    def __len__(self):
+        return len(self.videos)
+
+    def __getitem__(self, index):
+        video_idx = self.videos[index]
+        video_name = self.video_dict[int(video_idx)]
+
+        cnn2d_npy_path = os.path.join(self.root_path, f'video{video_idx}_cnn_2d_{self.cnn_2d_model}.npy')
+        cnn3d_npy_path = os.path.join(self.root_path, f'video{video_idx}_cnn_3d_{self.cnn_3d_model}.npy')
+        cnn2d_features = torch.FloatTensor(np.load(cnn2d_npy_path))
+        cnn3d_features = torch.FloatTensor(np.load(cnn3d_npy_path))
+        cnn_features = torch.cat((cnn3d_features, cnn2d_features), dim=0)
+
+        label = torch.zeros(len(self.tag_dict))
+        unique_words = set()
+        annotations = self.video_caption_mapping[video_name]
+        for annotation in annotations:
+            for token in annotation:
+                unique_words.add(token)
+        for word in unique_words:
+            if word in self.tag_dict:
+                label[self.tag_dict[word]] = 1.0
+
+        return (cnn_features, label)
