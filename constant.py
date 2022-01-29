4db09799fb878c343b14f10252f99bc8d488906d
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-29 21:55:32 +0700

    Add mean and std for kinetics dataset

--- constant.py
+++ constant.py
@@ -9,3 +9,6 @@ DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # https://pytorch.org/hub/pytorch_vision_vgg/
 IMAGE_MEAN = [0.485, 0.456, 0.406]
 IMAGE_STD = [0.229, 0.224, 0.225]
+
+KINETICS_MEAN = [110.63666788, 103.16065604, 96.29023126]
+KINETICS_STD = [1, 1, 1]
