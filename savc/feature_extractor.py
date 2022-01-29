8e9f9c6f20eb87dbc52b2a266473f946b52ea629
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-29 21:54:59 +0700

    Add feature extractor for 3d cnn

--- savc/feature_extractor.py
+++ savc/feature_extractor.py
@@ -3,8 +3,10 @@ import torch
 import numpy as np
 import torchvision.models as models
 from torch.utils.data import DataLoader
-from torchvision.transforms import RandomCrop, Normalize
-from savc.dataset import RawMSVDDataset
+from torchvision.transforms import RandomCrop, Normalize, Resize
+from savc.dataset import RawMSVDDataset, SequenceImageMSVDDataset
+from savc.models.shufflenet import get_model as ShuffleNet
+from savc.models.shufflenetv2 import get_model as ShuffleNetV2
 from constant import *
 from utils import build_video_dict
 
@@ -43,9 +45,53 @@ def extract_features_2d_cnn(annotations_file: str, root_path: str, output_dir: s
                 res += out_numpy
 
         res /= total_frames
-        npy_path = os.path.join(output_dir, f'video{video_index:04d}_cnn_2d.npy')
+        npy_path = os.path.join(output_dir, f'video{video_index:04d}_cnn_2d_regnetx32.npy')
         with open(npy_path, 'wb') as f:
             np.save(f, res)
+
+    print('\nFeature extraction complete')
+
+
+def extract_features_3d_cnn(
+    annotations_file: str,
+    root_path: str,
+    output_dir: str,
+    model_name: str = 'shufflenetv2',
+) -> None:
+    os.makedirs(output_dir, exist_ok=True)
+    if model_name == 'shufflenetv2':
+        model = ShuffleNetV2(
+            './checkpoints/shufflenet/kinetics_shufflenetv2_2.0x_RGB_16_best.pth',
+            width_mult=2.,
+        )
+    elif model_name == 'shufflenet':
+        model = ShuffleNet(
+            './checkpoints/shufflenet/kinetics_shufflenet_2.0x_G3_RGB_16_best.pth',
+            width_mult=2.,
+        )
+    else:
+        assert False, f'Unsupported model {model_name}'
+    model.eval()
+
+    all_videos = os.listdir(root_path)
+    video_dict = build_video_dict(annotations_file)
+    preprocess_funcs = [Normalize(KINETICS_MEAN, KINETICS_STD), Resize((112, 112))]
+
+    for idx, video in enumerate(all_videos):
+        print(f'Extracting video {idx+1}/{len(all_videos)}', end='\r')
+        video_index = video_dict[video]
+
+        raw_dataset = SequenceImageMSVDDataset(os.path.join(root_path, video), preprocess_funcs)
+        data_loader = DataLoader(raw_dataset, batch_size=1, shuffle=False)
+
+        for _, (X, _) in enumerate(data_loader):
+            X = X.to(DEVICE)
+            out = model(X)
+            out_numpy = out[0].cpu().detach().numpy()
+
+        npy_path = os.path.join(output_dir, f'video{video_index:04d}_cnn_3d_{model_name}.npy')
+        with open(npy_path, 'wb') as f:
+            np.save(f, out_numpy)
 
     print('\nFeature extraction complete')
 
@@ -87,9 +133,15 @@ def parse_features_from_txt(feature_file: str, output_dir: str) -> None:
 
 
 if __name__ == '__main__':
-    extract_features_2d_cnn(
+    # extract_features_2d_cnn(
+    #     'D:/ML Dataset/MSVD/annotations.txt',
+    #     'D:/ML Dataset/MSVD/YouTubeClips',
+    #     'D:/ML Dataset/MSVD/new_extracted',
+    #     batch_size=8,
+    # )
+    extract_features_3d_cnn(
         'D:/ML Dataset/MSVD/annotations.txt',
         'D:/ML Dataset/MSVD/YouTubeClips',
         'D:/ML Dataset/MSVD/new_extracted',
-        batch_size=8,
+        model_name='shufflenet',
     )
