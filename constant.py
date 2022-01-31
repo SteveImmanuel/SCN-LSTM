ce08b5e856df7011b85bd99576cbe7a4656d5694
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-31 21:30:06 +0700

    Add constant for semantic and cnn

--- constant.py
+++ constant.py
@@ -12,3 +12,15 @@ IMAGE_STD = [0.229, 0.224, 0.225]
 
 KINETICS_MEAN = [110.63666788, 103.16065604, 96.29023126]
 KINETICS_STD = [1, 1, 1]
+
+CNN_3D_FEATURES_SIZE = {
+    'shufflenet': 1920,
+    'shufflenetv2': 2048,
+}
+
+CNN_2D_FEATURES_SIZE = {
+    'vgg': 4096,
+    'regnetx32': 2520,
+}
+
+SEMANTIC_SIZE = 300
